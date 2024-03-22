3af02c66d43deb7c12e9b7f99b4495f2469b42d8
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-03-22 19:18:40 +0500

    fixed rerendering issue
    the re-rednering was casued because i was using the convex authentication components <Authenticated/> , <Unauthenticated/> and <Authloading/> when is changed to clerk auth system it seem to have fixed the issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { AuthLoading, Authenticated, Unauthenticated } from 'convex/react'
 import ThemeSwitch from './ThemeSwitch'
-import { SignInButton } from '@clerk/clerk-react'
+import { ClerkLoading, SignInButton, SignedIn, SignedOut } from '@clerk/clerk-react'
 import { Button } from './ui/button'
 import { UserButton, OrganizationSwitcher } from './ClerkComponents'
 import ToolTip from './Tooltip'
@@ -11,21 +11,21 @@ const Header = () => {
 		<div className='flex items-center justify-between mx-24 gap-5 border-b border-border p-4'>
 			Link
 			<div className='flex gap-8'>
-				<Authenticated>
+				<SignedIn>
 					<ToolTip message='Organization Switcher'>
 						<OrganizationSwitcher />
 					</ToolTip>
 					<UserButton />
-				</Authenticated>
-				<Unauthenticated>
+				</SignedIn>
+				<SignedOut>
 					<SignInButton mode='modal'>
 						<Button>Sign In</Button>
 					</SignInButton>
-				</Unauthenticated>
-				<AuthLoading>
+				</SignedOut>
+				<ClerkLoading>
 					<Skeleton className='w-44 h-10' />
 					<Skeleton className='w-10 h-10 rounded-full' />
-				</AuthLoading>
+				</ClerkLoading>
 				<ThemeSwitch />
 			</div>
 		</div>
