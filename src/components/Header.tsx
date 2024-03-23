d83931a17a19d9d0aa47ca0d38ec2b3fc6b895b4
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-03-23 06:16:49 +0500

    added responsiveness to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,36 @@
-import { AuthLoading, Authenticated, Unauthenticated } from 'convex/react'
 import ThemeSwitch from './ThemeSwitch'
-import { ClerkLoading, SignInButton, SignedIn, SignedOut } from '@clerk/clerk-react'
+import { ClerkLoading, OrganizationSwitcher, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
 import { Button } from './ui/button'
-import { UserButton, OrganizationSwitcher } from './ClerkComponents'
 import ToolTip from './Tooltip'
 import { Skeleton } from './ui/skeleton'
 
 const Header = () => {
 	return (
-		<div className='flex items-center justify-between mx-24 gap-5 border-b border-border p-4'>
-			Link
-			<div className='flex gap-8'>
-				<SignedIn>
-					<ToolTip message='Organization Switcher'>
-						<OrganizationSwitcher />
-					</ToolTip>
-					<UserButton />
-				</SignedIn>
-				<SignedOut>
-					<SignInButton mode='modal'>
-						<Button>Sign In</Button>
-					</SignInButton>
-				</SignedOut>
-				<ClerkLoading>
-					<Skeleton className='w-44 h-10' />
-					<Skeleton className='w-10 h-10 rounded-full' />
-				</ClerkLoading>
-				<ThemeSwitch />
+		<div className='w-full justify-center flex'>
+			<div className='flex items-center border-2 h-20 lg:w-[1024px] md:w-[770px] sm:w-[640px] xl:w-full xl:mx-24 justify-between gap-5 p-4'>
+				Link
+				<div className='flex gap-8'>
+					<SignedIn>
+						<div className='my-1'>
+							<ToolTip message='Organization Switcher'>
+								<OrganizationSwitcher />
+							</ToolTip>
+						</div>
+						<div className='my-1'>
+							<UserButton />
+						</div>
+					</SignedIn>
+					<SignedOut>
+						<SignInButton mode='modal'>
+							<Button>Sign In</Button>
+						</SignInButton>
+					</SignedOut>
+					<ClerkLoading>
+						<Skeleton className='w-44 h-10' />
+						<Skeleton className='w-10 h-10 rounded-full' />
+					</ClerkLoading>
+					<ThemeSwitch />
+				</div>
 			</div>
 		</div>
 	)
