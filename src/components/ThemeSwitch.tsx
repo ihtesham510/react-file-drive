7d955592db250fcc5822b41b7ee15e95866270b9
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-03-23 06:16:37 +0500

    added toolTip to theme switcher

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,20 +1,23 @@
 import { Button } from './ui/button'
 import { useTheme } from './ThemeContext'
 import { SunIcon, MoonIcon } from 'lucide-react'
+import ToolTip from './Tooltip'
 
 export default function ThemeSwitch() {
 	const { theme, toggleTheme } = useTheme()
 
 	return (
-		<Button variant='outline' onClick={() => toggleTheme()} size='icon'>
-			{theme == 'light' && (
-				<SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-			)}
-			<MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-			{theme == 'dark' && (
+		<ToolTip message={`Switch to ${theme == 'light' ? 'dark' : 'light'}`}>
+			<Button variant='outline' onClick={() => toggleTheme()} size='icon'>
+				{theme == 'light' && (
+					<SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+				)}
 				<MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-			)}
-			<span className='sr-only'>Toggle theme</span>
-		</Button>
+				{theme == 'dark' && (
+					<MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+				)}
+				<span className='sr-only'>Toggle theme</span>
+			</Button>
+		</ToolTip>
 	)
 }
