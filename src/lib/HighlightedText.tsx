748c472602d594d0f414af06db2d6afbe05f20b2
Ihtesham ul haq <103510255+ihtesham510@users.noreply.github.com>
2024-04-18 13:39:26 +0500

    Update HighlightedText.tsx

--- src/lib/HighlightedText.tsx
+++
@@ -1,9 +0,0 @@
-interface HighlightProps {
-	text: string
-	value: string
-}
-export const HighlightedText: React.FC<HighlightProps> = ({ text, value }) => {
-	const textArr = text.split('')
-	const valueArr = value.split('')
-	return <></>
-}
