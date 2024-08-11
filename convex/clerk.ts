df5d5d02c5e9e33e27c849bac04ce8e056ab5abf
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-08-11 12:51:30 +0500

    added env

--- convex/clerk.ts
+++ convex/clerk.ts
@@ -6,7 +6,7 @@ import { internalAction } from './_generated/server'
 export const fulfill = internalAction({
 	args: { headers: v.any(), payload: v.string() },
 	handler: async (ctx, args) => {
-		const wh = new Webhook(import.meta.env.CLERK_WEBHOOK_SECRET)
+		const wh = new Webhook(`whsec_KsHZzust82LTnST17WUkXsjssI/XrVgX`)
 		const payload = wh.verify(args.payload, args.headers) as WebhookEvent
 		return payload
 	},
