780ca5425726f1d852847e7b74c13ad347eeb53f
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-08-13 20:37:49 +0500

    added signout orl

--- src/components/ClerkComponents.tsx
+++ src/components/ClerkComponents.tsx
@@ -4,5 +4,5 @@ import {
 	OrganizationSwitcher as OriginalOrganizationSwitcher,
 } from '@clerk/clerk-react'
 
-export const UserButton = React.memo(props => <OriginalUserButton {...props} />)
+export const UserButton = React.memo(props => <OriginalUserButton {...props} afterSignOutUrl='/' />)
 export const OrganizationSwitcher = React.memo(props => <OriginalOrganizationSwitcher {...props} />)
