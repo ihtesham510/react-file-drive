f0c5d4996357749683d2d1dc1374b05d15ba35b5
ihtesham510 <ihteshamulhaq510@gmail.com>
2024-10-06 11:54:46 +0500

    fixed spacing issue in mobile view

--- src/Pages/Dashboard/Header.tsx
+++ src/Pages/Dashboard/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
 	const location = useLocation()
 	const isActive = (path: string) => location.pathname.split('/').includes(path)
 	return (
-		<div className='flex sticky lg:static top-0 z-50 lg:bg-background backdrop-blur py-4 items-center mx-2 lg:mx-5 sm:h-20 justify-between sm:justify-between'>
+		<div className='flex sticky lg:static top-0 z-50 lg:bg-background backdrop-blur py-4 items-center mx-2 lg:mx-5 sm:h-20 justify-between'>
 			<div className='lg:hidden'>
 				<SideBarSheet>
 					<Button variant='ghost' size='sm'>
@@ -17,20 +17,24 @@ const Header = () => {
 					</Button>
 				</SideBarSheet>
 			</div>
+
 			<div className='hidden lg:block w-[300px]'>
+				<h1 className='text-2xl font-bold'>React File Drive</h1>
+			</div>
+
+			<div className='hidden lg:block'>
 				<h1 className='text-2xl font-semibold '>
 					{isActive('allfiles') && 'All Files'}
 					{isActive('favorites') && 'Favorites Files'}
 					{isActive('trash') && 'Trashed Files'}
 				</h1>
 			</div>
-			<div className='hidden lg:block'>
-				<h1 className='text-2xl font-bold'>React File Drive</h1>
-			</div>
-			<div className='flex lg:hidden mt-2'>
+
+			<div className='lg:hidden mt-2'>
 				<OrganizationSwitcher />
 			</div>
-			<div className='flex justify-end items-center gap-5 w-[400px] mr-2'>
+
+			<div className='flex justify-end items-center gap-5 lg:w-[400px] mr-2'>
 				<div className='mt-2 hidden lg:block mr-2'>
 					<OrganizationSwitcher />
 				</div>
